7ffa9f42290a6f5c48242f8b18feb9dcff9e4e9f
tangshiw <cukpuk@gmail.com>
2020-07-08 08:32:15 -0700

    Updates

---
+++ precache-manifest.cddd445fde83e3374c7b17cd7eea0783.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d4294d2771799bc20c6130503a95fee",
+    "url": "/AnswerStats/index.html"
+  },
+  {
+    "revision": "c86532a94ef3dc870455",
+    "url": "/AnswerStats/static/css/main.453dc7aa.chunk.css"
+  },
+  {
+    "revision": "8557f795b97b82a5589f",
+    "url": "/AnswerStats/static/js/2.bccc8a09.chunk.js"
+  },
+  {
+    "revision": "ee55ef9819cc22f2caed880db5957f83",
+    "url": "/AnswerStats/static/js/2.bccc8a09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c86532a94ef3dc870455",
+    "url": "/AnswerStats/static/js/main.e59215bb.chunk.js"
+  },
+  {
+    "revision": "411ddd5142be9eb7a023",
+    "url": "/AnswerStats/static/js/runtime-main.da64a127.js"
+  }
+]);
