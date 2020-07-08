a46a7bddf3c2d799f0eca3442ade380f966a7cca
tangshiw <cukpuk@gmail.com>
2020-07-08 09:23:16 -0700

    second

--- src/container/index.js
+++ src/container/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css'
 import { makeStyles } from '@material-ui/core/styles';
 import {
@@ -13,6 +13,7 @@ import TopHeader from '../component/TopHeader'
 import Home from './Home'
 import Questions from '../Data/Questions'
 import Question from './SingleQuestion/index'
+import { Background } from 'devextreme-react/range-selector';
 const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
@@ -27,26 +28,31 @@ const useStyles = makeStyles((theme) => ({
   }));
 
 function App() {
-
     const classes = useStyles();
+    const [Clicked, setClicked] = useState(0)
+
         return (
             <div>
                 <TopHeader />
                 <Router>
                     <div>
                         <div className={classes.root}>
-                            <div className="circleBase type1">
-                                <Link to="/" style={{ color:"white", textDecoration: "none"}}>
+                            <div className="circleBase type1" style={{ background: Clicked===0 ? "blue" : null}}>
+                                <Link to="/" style={{ color:"white", textDecoration: "none"}} onClick={()=>setClicked(0)}>
                                     全部
                                 </Link>
                             </div>
                             {Questions.map(item => {
                                 return (
                                     <div key={item.id} className="navContainer">
-                                        <div className="circleBase type1">
-                                            <Link to={`/${item.id}`} style={{ color:"white", textDecoration: "none"}}>{item.Name}</Link>
+                                        <div className="circleBase type1" style={{ background: Clicked===item.id ? "blue" : null}}>
+                                            <Link to={`/${item.id}`} style={{ color:"white", textDecoration: "none"}} onClick={()=>setClicked(item.id)}>
+                                                {item.Name}
+                                            </Link>
                                         </div>
-                                        <span className={classes.Correct_Rate} className="CorrectRate">{Math.round((item.Correct/(item.Correct+item.Wrong))*100)}%</span>
+                                        <span className={classes.Correct_Rate} className="CorrectRate">
+                                            {Math.round((item.Correct/(item.Correct+item.Wrong))*100)}%
+                                        </span>
                                     </div>
                                 )
                             })}
